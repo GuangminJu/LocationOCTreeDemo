a67dfaf118680db7f274e9ecd1077c2a70757a12
Mellos <1429326774@qq.com>
2025-02-23 14:06:51 +0800

    Add test

---
+++ Source/LocationOCTreeDemo/BPLib.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BPLib.h"
+
+#include "Kismet/GameplayStatics.h"
+
+AActor* UBPLib::GetNearlyActorForeach(const FVector& InLocation, float InRadius, TSubclassOf<AActor> InActorClass,
+                                      const UObject* InWorldContextObject, bool bDrawDebug)
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE(UBPLib::GetNearlyActorForeach);
+
+	UWorld* World = InWorldContextObject->GetWorld();
+	if (!World)
+	{
+		return nullptr;
+	}
+
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsOfClass(World, InActorClass, FoundActors);
+	AActor* NearlyActor = nullptr;
+
+	for (AActor* Actor : FoundActors)
+	{
+		if (!NearlyActor)
+		{
+			NearlyActor = Actor;
+			continue;
+		}
+
+		if (FVector::DistSquared(Actor->GetActorLocation(), InLocation) < FVector::DistSquared(
+			NearlyActor->GetActorLocation(), InLocation))
+		{
+			NearlyActor = Actor;
+		}
+
+#if !UE_BUILD_SHIPPING
+		if (bDrawDebug)
+		{
+			DrawDebugLine(World, InLocation, Actor->GetActorLocation(), FColor::Blue,
+				false, 0.1f, 0, 5);
+		}
+#endif
+	}
+
+	return NearlyActor;
+}
---
+++ Source/LocationOCTreeDemo/BPLib.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "BPLib.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOCATIONOCTREEDEMO_API UBPLib : public UObject
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintCallable, Category = "BPLib", meta = (WorldContext = "InWorldContextObject"))
+	static AActor* GetNearlyActorForeach(const FVector& InLocation, float InRadius, TSubclassOf<AActor> InActorClass,
+	                                     const UObject* InWorldContextObject, bool bDrawDebug = false);
+};
